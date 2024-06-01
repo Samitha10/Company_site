4e1a909539fece5620b1801357f028aa1aa7c96b
Sandeepa Sineth <sineth21404@gmail.com>
2024-06-01 13:03:28 +0530

    Color Configuration Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'blue-bg' :'#00003C',
+      'gray-1'  :'#7f7f9d',
+      'gray-2'  :'#bfbfce',
+      'gray-3'  :'#e6e6ec',
+      'gray-4'  :'#f2f2f5',
+      'purple-1':'#afa1eb',
+      'purple-2':'#d6cff4',
+      'purple-3':'#ebe7fa',
+      'green-1' :'#bff6c2',
+      'green-2' :'#bff6c2',
+      'green-3' :'#dffae1',
+      'blue-1'  :'#3da5ff',
+      'blue-1'  :'#00003C',
+      'blue-1'  :'#cee8ff',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
