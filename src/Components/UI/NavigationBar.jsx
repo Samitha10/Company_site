9d99860889510e3248349ab0167bf4af0f82efec
Sandeepa Sineth Wickramasinghe <101142560+Darkhenrhal@users.noreply.github.com>
2024-06-14 12:27:01 +0000

    Home page : Hero Completed : V1

--- src/Components/UI/NavigationBar.jsx
+++ src/Components/UI/NavigationBar.jsx
@@ -1,44 +1,39 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { BsTwitterX } from "react-icons/bs";
+
+
 
 const NavigationBar = () => {
     return (
-            <div id="navigationBar" class="z-999 flex flex-wrap place-items-center top-0">
-            <section class="relative mx-auto">
+            // <div id="navigationBar" className="z-999 flex flex-wrap place-items-center opacity-98 top-0 bg-gradient-to-r from-purple-bg to-blue-bg">
+            <div id="navigationBar" className="z-999 flex flex-wrap place-items-center w-fill opacity-100 top-0 bg-gray-4">
+
+            <section class="relative mx-auto ">
             
-                <nav class="flex justify-between bg-gray-900 text-white w-screen">
-                <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-                    <a class="text-3xl font-bold font-heading text-gray-4" href="#">
+                <nav class="flex justify-between bg-gray-900 text-white w-screen ">
+                <div class="px-5 xl:px-12 py-2 flex w-full items-center">
+                    <a class="text-lg font-bold font-heading text-gray-3" href="#">
                      {/* <img class="h-9" src="logo.png" alt="logo">  */}
                     Logo Here.
                     </a>
                     <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-                    <li><a class="hover:text-gray-4 text-gray-2 font-Poppins" href="#">Home</a></li>
-                    <li><a class="hover:text-gray-4 text-gray-2 font-Poppins"  href="#">Catagory</a></li>
-                    <li><a class="hover:text-gray-4 text-gray-2 font-Poppins"  href="#">Collections</a></li>
-                    <li><a class="hover:text-gray-4 text-gray-2 font-Poppins"  href="#">Contact Us</a></li>
+                    <li><a class="hover:text-blue-bg text-gray-2 font-Poppins" href="#">Home</a></li>
+                    <li><a class="hover:text-gray-1 text-gray-2 font-Poppins"  href="#">Catagory</a></li>
+                    <li><a class="hover:text-gray-1 text-gray-2 font-Poppins"  href="#">Collections</a></li>
+                    <li><a class="hover:text-gray-1 text-gray-2 font-Poppins"  href="#">Contact Us</a></li>
                     </ul>
-                    <div class="hidden xl:flex items-center space-x-5 items-center">
-                    <a class="hover:text-gray-200" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                        </svg>
+                    <div className="hidden xl:flex items-center space-x-5 ">
+                    <a className="hover:text-gray-200 hover:text-gray-1 text-gray-2" href="#">
+                        <BsLinkedin />
                     </a>
-                    <a class="flex items-center hover:text-gray-200" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                        </svg>
-                        <span class="flex absolute -mt-5 ml-4">
-                        <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                            <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-                            </span>
-                        </span>
+                    <a className="flex items-center hover:text-gray-200 hover:text-gray-1 text-gray-2" href="#">
+                        <BsFacebook />
                     </a>
-                    <a class="flex items-center hover:text-gray-200" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
+                    <a className="flex items-center hover:text-gray-200 hover:text-gray-1 text-gray-2" href="#">
+                        <BsTwitterX />
                     </a>
-                    
                     </div>
                 </div>
                  {/* Responsive navbar  */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,25 @@ export default {
   ],
   theme: {
     colors: {
+     
+      'base-gray':'#444445',
       'blue-bg' :'#00003C',
       'purple-bg':'#1b033a',
-      'gray-1'  :'#7f7f9d',
-      'gray-2'  :'#bfbfce',
-      'gray-3'  :'#e6e6ec',
-      'gray-4'  :'#f2f2f5',
-      'purple-1':'#afa1eb',
-      'purple-2':'#d6cff4',
-      'purple-3':'#ebe7fa',
-      'green-1' :'#80ee87',
-      'green-2' :'#bff6c2',
-      'green-3' :'#dffae1',
-      'blue-1'  :'#3da5ff',
-      'blue-2'  :'#9ed2ff',
-      'blue-3'  :'#cee8ff',
+      'gray-1':'#29293F',
+      'gray-2'  :'#424263',
+      'gray-3'  :'#8888a5',
+      'gray-4'  :'#e6e6ec',
+      'gray-5'  :'#d9d9d9',
+      'gray-6'  :'#f2f2f5',
+      'purple-1':'#4B3A98',
+      'purple-2':'#8A78D8',
+      'purple-3':'#B6AAEC',
+      'green-1' :'#23AE2C',
+      'green-2' :'#4EC656',
+      'green-3' :'#80EE87',
+      'blue-1'  :'#0F85EC',
+      'blue-2'  :'#53A4EB',
+      'blue-3'  :'#93C8F5',
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
@@ -39,7 +43,7 @@ export default {
         '4xl': '2rem',
       },
       backgroundImage: {
-        'hero-home': "url('./public/Meteor.svg')",
+        'hero-home': "url('data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' width='4096' height='2160' preserveAspectRatio='none' viewBox='0 0 4096 2160'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1024%26quot%3b)' fill='none'%3e%3cpath d='M4326.86 853.14C3823.52 858.79 3406.7 1428.99 2393.26 1414.74 1379.82 1400.49 1103.84-235.72 459.66-363.84' stroke='rgba(51%2c121%2c194%2c0.58)' stroke-width='2'%3e%3c/path%3e%3cpath d='M4827.48 762.74C4339.85 779.29 3966.66 1549.67 3037.42 1540.34 2108.19 1531.01 1861.85-95.19 1247.37-237.16' stroke='rgba(51%2c121%2c194%2c0.58)' stroke-width='2'%3e%3c/path%3e%3cpath d='M4486.85 884.75C3999.74 918.46 3689.35 1873.36 2792.79 1856.75 1896.23 1840.14 1742.78-61.74 1098.73-292.57' stroke='rgba(51%2c121%2c194%2c0.58)' stroke-width='2'%3e%3c/path%3e%3cpath d='M4470.47 759.49C4169.41 757.76 3883.66 489.49 3296.85 489.49 2710.04 489.49 2725.45 762.99 2123.23 759.49 1521.01 755.99 1304.28-13.83 949.61-52.06' stroke='rgba(51%2c121%2c194%2c0.58)' stroke-width='2'%3e%3c/path%3e%3cpath d='M4237.75 508.44C3951.16 517.94 3740.64 971.8 3184.24 962.04 2627.85 952.28 2509.51-101.4 2130.74-209.4' stroke='rgba(51%2c121%2c194%2c0.58)' stroke-width='2'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1024'%3e%3crect width='4096' height='2160' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e')",
         'footer-texture': "url('/img/footer-texture.png')",
       }
     }
